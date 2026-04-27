742e05ea1db3730246368997aabc26fe011fa047
pedrogois <phjgoiis@gmail.com>
2026-04-27 18:40:26 -0300

    testando projeto

---
+++ src/aplication/Main.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position posicao = new Position(1, 3);
+
+        System.out.println(posicao);
+
+    }
+}
